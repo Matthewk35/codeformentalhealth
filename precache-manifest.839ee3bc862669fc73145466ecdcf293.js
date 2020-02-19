92090812d560dca565b9a17bf58fb4c81ce5dd24
Matthewk35 <matthewkempa@hotmail.co.uk>
2020-02-19 03:27:54 +0000

    Updates

---
+++ precache-manifest.839ee3bc862669fc73145466ecdcf293.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "705aa7d5e87faff82aa9093c07746374",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ada88931d3657f6bc24",
+    "url": "/static/css/main.fabca7f7.chunk.css"
+  },
+  {
+    "revision": "fd0105da88a972d9f15a",
+    "url": "/static/js/2.e74ad889.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.e74ad889.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5ada88931d3657f6bc24",
+    "url": "/static/js/main.fb5e8e9a.chunk.js"
+  },
+  {
+    "revision": "afd62370e86a6aa9839d",
+    "url": "/static/js/runtime-main.956d7753.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/codeformentalhealth/precache-manifest.070148907b9cb36c830207f250cdb824.js"
+  "/precache-manifest.839ee3bc862669fc73145466ecdcf293.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/codeformentalhealth/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
